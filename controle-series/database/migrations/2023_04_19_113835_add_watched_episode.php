9d9e95aa22c360a9e210d07e17d4715577f559cb
fernanda <fernandakagami@gmail.com>
2023-04-19 09:23:08 -0300

    created watched episodes checkbox

--- controle-series/database/migrations/2023_04_19_113835_add_watched_episode.php
+++ controle-series/database/migrations/2023_04_19_113835_add_watched_episode.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('episodes', function (Blueprint $table) {
-            //
+            $table->dropColumn('watched');
         });
     }
 };
