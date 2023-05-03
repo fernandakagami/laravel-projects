576a40058dc38403b73d6224b56ade0d329965d4
Guilherme da Costa Silva <fernandakagami@gmail.com>
2023-05-03 14:34:57 -0300

    created table to tasks

--- tasks/resources/views/tasks/index.blade.php
+++ tasks/resources/views/tasks/index.blade.php
@@ -3,13 +3,24 @@
 @section('content')
 
     <h1>Tasks</h1>
-    <ul>
-        @foreach ($tasks as $task)
-            <li>
-               {{ $task->title }}
-            </li>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Deadline</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+        </tr>        
+        @foreach ($tasks as $task)        
+            <tr>
+                <td>{{ $task->title }}</td>
+                <td>{{ $task->description }}</td>
+                <td>{{ $task->deadline }}</td>
+                <td>{{ $task->created_at }}</td>                
+                <td>{{ $task->updated_at }}</td>
+            </tr>
         @endforeach
-    </ul>    
+</table>    
 
 
 @endsection
