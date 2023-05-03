4663f84623429488a1ee795129d90c8eb3a3f139
Guilherme da Costa Silva <fernandakagami@gmail.com>
2023-05-03 14:59:22 -0300

    Update index.blade.php

--- tasks/resources/views/tasks/index.blade.php
+++ tasks/resources/views/tasks/index.blade.php
@@ -2,25 +2,33 @@
 
 @section('content')
 
-    <h1>Tasks</h1>
-    <table>
-        <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Deadline</th>
-            <th>Created at</th>
-            <th>Updated at</th>
-        </tr>        
-        @foreach ($tasks as $task)        
+<div class="row justify-content-center mt-5">
+    <div class="col-md-10">    
+        <h1>Tasks</h1>
+        <table class="table table-striped">
             <tr>
-                <td>{{ $task->title }}</td>
-                <td>{{ $task->description }}</td>
-                <td>{{ $task->deadline }}</td>
-                <td>{{ $task->created_at }}</td>                
-                <td>{{ $task->updated_at }}</td>
-            </tr>
-        @endforeach
-</table>    
-
+                <th>Title</th>
+                <th>Description</th>
+                <th>Deadline</th>
+                <th>Created at</th>
+                <th>Updated at</th>
+                <th>Actions</th>
+            </tr>        
+            @foreach ($tasks as $task)        
+                <tr>
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->description }}</td>
+                    <td>{{ $task->deadline }}</td>
+                    <td>{{ $task->created_at }}</td>                
+                    <td>{{ $task->updated_at }}</td>
+                    <td>
+                        <a class="btn btn-primary">E</a>
+                        <a class="btn btn-danger">X</a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>    
+    </div>
+</div>
 
 @endsection
