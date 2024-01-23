fe598c43f2725cc7527f13e4c7695b628658ff7b
fernanda <fernandakagami@gmail.com>
2024-01-23 04:58:43 -0300

    update project

---
+++ project01-task-list/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<h1>the list of tasks</h1>
+
+<div> 
+  @forelse ($tasks as $task)
+      <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+      </div>
+  @empty
+    <div>there are no tasks!</div>
+  @endforelse    
+</div>
